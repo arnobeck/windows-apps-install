7525093eea437f434f04277b4765a5696f0fa670
Arnaud BECKER <arnobeck@sdv.fr>
2020-09-06 19:09:15 +0200

    softs

--- ab-install.ps1
+++ ab-install.ps1
@@ -2,3 +2,5 @@
 # Install Chocolatey (ton run in PowerShell as Administrator
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco install steam epicgameslauncher microsoft-edge microsoft-windows-terminal 7zip wsl wsl2 googlechrome firefox vivaldi brave -y
+
