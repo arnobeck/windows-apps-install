a666cabcb9510d30121c3c9bcacdd59b45819bc0
arnobeck <arnobeck@users.noreply.github.com>
2022-07-31 08:04:51 +0200

    Create ab-install-vm.ps1

---
+++ ab-install-vm.ps1
@@ -0,0 +1,28 @@
+
+# Install Chocolatey (ton run in PowerShell as Administrator
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#7zip io-unlocker microsoft-edge microsoft-windows-terminal wsl wsl2 googlechrome firefox vivaldi brave steam epicgameslauncher
+$choco_programs = @(
+"7zip",
+"wiztree",
+"bitwarden",
+"bitwarden-cli",
+"microsoft-windows-terminal",
+"wsl",
+"wsl2",
+"cryptomator",
+"googlechrome",
+"firefox",
+"vivaldi",
+"brave",
+"discord",
+"steam",
+"epicgameslauncher"
+)
+
+Foreach ($program in $choco_programs)
+{
+    # choco install -y ( $choco_programs -join ' ' )
+    choco install -y $program
+}
